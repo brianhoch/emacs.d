554bc2f6e3380998a6d7ee8bc9d20cb3d902f96d
Brian Hochhalter <brian.hochhalter@gmail.com>
2019-03-18 23:06:41 -0400

    Initial commit

---
+++ init.el
@@ -0,0 +1,76 @@
+(require 'package)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" .
+                                 "https://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Find executables
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+;; ### AUTO-GENERATED -- DO NOT HAND ALTER ###
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (wombat)))
+ '(global-display-line-numbers-mode t)
+ '(package-selected-packages
+   (quote
+    (evil-matchit sentence-navigation evil-surround web-mode exec-path-from-shell flycheck markdown-mode org-make-toc quelpa-use-package use-package evil)))
+ '(save-place t)
+ '(sentence-end-double-space nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(visible-bell t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; End ### AUTO-GENERATED -- DO NOT HAND ALTER ###
+
+;; Vim emulation, please
+(require 'evil)
+(evil-mode 1)
+
+;; check code syntax
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; Turn off button bar in GUI Emacs
+(tool-bar-mode -1)
+
+;; Show line numbers for vim `{ line number } G` convenience
+(when (version<= "26.0.50" emacs-version )
+  (global-display-line-numbers-mode))
+
+;; Export formats from Org mode
+(require 'ox-md) ;; Markdown
+
+;; Set base font
+(add-to-list 'default-frame-alist
+                       '(font . "Input Mono-16"))
+;; Force UTF-8
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
+;; Remap org-mode meta keys for convenience
+(mapcar (lambda (state)
+    (evil-declare-key state org-mode-map
+      (kbd "M-l") 'org-metaright
+      (kbd "M-h") 'org-metaleft
+      (kbd "M-k") 'org-metaup
+      (kbd "M-j") 'org-metadown
+      (kbd "M-L") 'org-shiftmetaright
+      (kbd "M-H") 'org-shiftmetaleft
+      (kbd "M-K") 'org-shiftmetaup
+      (kbd "M-J") 'org-shiftmetadown))
+  '(normal insert))
