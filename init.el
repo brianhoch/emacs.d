b458299ac99df579183cf6e66ae482b4e635597c
Brian Hochhalter <brian.hochhalter@macmillan.com>
2019-04-10 14:01:12 -0400

    Add cdl.org to agenda

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
  '(indicate-empty-lines t)
  '(org-agenda-files
    (quote
-    ("~/Nextcloud/orgs/designops_tea-2019-04.org" "~/Nextcloud/orgs/it.org" "~/Nextcloud/orgs/goals_2019.org" "~/Nextcloud/orgs/aaron_1on1.org" "~/Nextcloud/orgs/work.org" "~/Nextcloud/orgs/home.org" "~/Nextcloud/orgs/ministry.org")))
+    ("~/Nextcloud/orgs/cdl.org" "~/Nextcloud/orgs/designops_tea-2019-04.org" "~/Nextcloud/orgs/it.org" "~/Nextcloud/orgs/goals_2019.org" "~/Nextcloud/orgs/aaron_1on1.org" "~/Nextcloud/orgs/work.org" "~/Nextcloud/orgs/home.org" "~/Nextcloud/orgs/ministry.org")))
  '(package-selected-packages
    (quote
     (evil-tutor evil-magit magit evil-matchit sentence-navigation evil-surround web-mode exec-path-from-shell flycheck markdown-mode quelpa-use-package use-package evil)))
